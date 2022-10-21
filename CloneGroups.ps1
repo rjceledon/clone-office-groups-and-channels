4c67965d660d8d7bdce129f4165533b8161280bb
rjceledon <85322110+rjceledon@users.noreply.github.com>
2022-10-21 10:18:05 -0400

    Update CloneGroups.ps1
    Adding log capability and increased error message readability.

--- CloneGroups.ps1
+++ CloneGroups.ps1
@@ -1,10 +1,13 @@
 $UserID = $args[0]
-$TargetID = $args[1]
-
+$TargetUserID = $args[1]
+Write-Host $UserID $TargetUserID
 $ExitMsg = "`n    [!] Exiting...`n"
+$Date = Get-Date -Format "MMddyyyyHHmm"
+$Log = "CloneGroupsLog" + $Date + ".txt"
+
 
 #Arg checking
-If (($UserID -eq $null) -or ($TargetID -eq $null)) {
+If (($UserID -eq $null) -or ($TargetUserID -eq $null)) {
     Write-Host ("`nThis script copies O365 groups and MSTeams Team Channels from <USER-EMAIL> to <TARGET-EMAIL>`nUser should be all set within the Office portal.`n`n[!] Usage: .\" + $MyInvocation.MyCommand.Name + " <USER-EMAIL> <TARGET-EMAIL>")
     Write-Host -F Red $ExitMsg
     Exit 1
@@ -13,7 +16,7 @@ If (($UserID -eq $null) -or ($TargetID -eq $null)) {
 #Prompt
 Write-Host "[-] Copying information"
 $Title = "Copying information"
-$Message = "Do you really want to copy all O365 and Team Channels information from " + $UserID + " to " + $TargetID + "?`nThis action can't be reverted."
+$Message = "Do you really want to copy all O365 and Team Channels information from " + $UserID + " to " + $TargetUserID + "?`nThis action can't be reverted."
 $Yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Copies all information."
 $No = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Exit."
 $Options = [System.Management.Automation.Host.ChoiceDescription[]]($Yes, $No)
@@ -45,12 +48,20 @@ $TargetUser = Get-AzureADUser -ObjectId $TargetUserID
 Write-Host "[+] Getting groups data"
 $Memberships = Get-AzureADUserMembership -ObjectId $User.ObjectId | Where-Object { $_.ObjectType -eq "Group" }
 $Memberships | ForEach-Object {
-    Try {
-        Write-Host "[+] Adding user" $TargetUser.UserPrincipalName "to group" $_.Mail
-        Add-AzureADGroupMember -ObjectId $_.ObjectId -RefObjectId $TargetUser.ObjectId
-    }
-    Catch {
-        Write-Host -F Red "    [!] Error Adding User to group:" $_.Exception.Message
+    $CurrentGroup = $_
+    If ( -not ($CurrentGroup.Mail -eq $null) ) {
+        Try {
+            Write-Host "[+] Adding user" $TargetUser.UserPrincipalName "to group" $_.Mail
+            Add-AzureADGroupMember -ObjectId $_.ObjectId -RefObjectId $TargetUser.ObjectId
+        }
+        Catch {
+            $ErrorMsg = ($_.Exception.Message -Split '\n')[2] -Replace "Message: "
+            $Error = "    [!] Error Adding " + $TargetUserID + " to group " + $CurrentGroup.Mail + ": " + $ErrorMsg
+            Write-Host -F Red $Error
+            $Date = Get-Date -Format "MM/dd/yyyy HH:mm"
+            $Error = $Date + ": " + $Error
+            Out-File -Filepath $log -Append -InputObject $Error
+        }
     }
 }
 Write-Host "`n"
@@ -66,35 +77,49 @@ Catch {
     Exit 1
 }
 
+
+
 #Get Teams, then Channels and iterate
 $Teams = Get-Team -User $UserID
 $Teams | ForEach-Object {
     $CurrentGroup = $_
     Write-host "[+] Found team:" $CurrentGroup.DisplayName
+
+    #Add user to Team
+    Try {
+        Write-Host "[+] Adding user" $TargetUserID "to team" $CurrentGroup.DisplayName
+        Add-TeamUser -GroupId $CurrentGroup.GroupId -User $TargetUserID
+    }
+    Catch {
+        $ErrorMsg = ($_.Exception.Message -Split '\n')[2] -Replace "Message: "
+        $Error = "    [!] Error Adding " + $TargetUserID + " to team " + $CurrentGroup.DisplayName + ": " + $ErrorMsg
+        Write-Host -F Red $Error
+        $Date = Get-Date -Format "MM/dd/yyyy HH:mm"
+        $Error = $Date + ": " + $Error
+        Out-File -Filepath $log -Append -InputObject $Error
+    }
     $Channel = Get-TeamChannel -GroupId $CurrentGroup.GroupId
     $Channel | ForEach-Object {
         $CurrentChannel = $_
-        Get-TeamChannelUser -GroupId $CurrentGroup.GroupId -DisplayName $CurrentChannel.DisplayName | % {
-            If ($_.User -eq $UserID) {
-                Write-Host "[+]" $UserID "is member of" $CurrentChannel.DisplayName
-                
-                #Add user to Team
-                Try {
-                    Write-Host "[+] Adding user" $TargetUserID "to team" $CurrentGroup.DisplayName
-                    Add-TeamUser -GroupId $CurrentGroupId -User $TargetUserID
-                }
-                Catch {
-                    Write-Host -F Red "    [!] Error Adding User to team:" $CurrentGroup.DisplayName
-                }
-                
-                #Add user to Channel
-                Try {
-                    Write-Host "[+] Adding user" $TargetUserID "to channel" $CurrentChannel.DisplayName "-" $CurrentGroup.DisplayName
-                    Add-TeamChannelUser -GroupId $CurrentGroup.GroupId -DisplayName $CurrentChannel.DisplayName -User $TargetUserID
-                }
-                Catch {
-                    Write-Host -F Red "    [!] Error Adding User to channel:" $CurrentChannel.DisplayName
-                }
+        $ChannelUsers = Get-TeamChannelUser -GroupId $CurrentGroup.GroupId -DisplayName $CurrentChannel.DisplayName
+        If ( ($ChannelUsers.User -Contains $UserID) -and ($ChannelUsers.User -Contains $TargetUserID) ) {
+            Write-Host -F Green "    [+]" $UserID "and" $TargetUserID "are both member of" $CurrentChannel.DisplayName
+        }
+        ElseIf ( ($ChannelUsers.User -Contains $UserID) -and ( -not ($ChannelUsers.User -Contains $TargetUserID) ) ) {
+            Write-Host -F Red "    [+]" $UserID "not" $TargetUserID "are both member of" $CurrentChannel.DisplayName
+
+            #Add user to Channel
+            Write-Host "[+] Adding user" $TargetUserID "to channel" $CurrentChannel.DisplayName "-" $CurrentGroup.DisplayName
+            Try { 
+                Add-TeamChannelUser -GroupId $CurrentGroup.GroupId -DisplayName $CurrentChannel.DisplayName -User $TargetUserID
+            }
+            Catch {
+                $ErrorMsg = ($_.Exception.Message -Split '\n')[2] -Replace "Message: "
+                $Error = "    [!] Error Adding " + $TargetUserID + " to channel " + $CurrentChannel.DisplayName + ": " + $ErrorMsg
+                Write-Host -F Red $Error
+                $Date = Get-Date -Format "MM/dd/yyyy HH:mm"
+                $Error = $Date + ": " + $Error
+                Out-File -Filepath $log -Append -InputObject $ErrorA        
             }
         }
     }
